f1f4120f25a0d7e7ee321e3d74f05dee68567efc
Khadijeh Mohammadi <khadijehmohammadi84@gmail.com>
2025-08-22 22:50:12 +0330

    task1

---
+++ test1/calculator.js
@@ -0,0 +1,7 @@
+let num1 = 5;
+let num2 = 2;
+console.log(`addition: ${num1 + num2}`);
+console.log(`subtraction: ${num1 - num2}`);
+console.log(`multiplication: ${num1 * num2}`);
+console.log(`division: ${num1 / num2}`);
+console.log(`remainder: ${num1 % num2}`);
---
+++ test1/traffic.js
@@ -0,0 +1,10 @@
+let trafficLightColor = "red";
+if (trafficLightColor === "red") {
+  console.log("stop");
+} else if (trafficLightColor === "yellow") {
+  console.log("slow down");
+} else if (trafficLightColor === "green") {
+  console.log("go");
+} else {
+  console.log("invalid color");
+}
